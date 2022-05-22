c076072e6841132f790611fce929290fb0bde0b1
tringakrs <94629975+tringakrs@users.noreply.github.com>
2022-05-22 02:05:27 +0200

    Add files via upload

---
+++ Dojo/Assignments/Week 3/Session 1/VideoPreview/video-preview/script.js
@@ -0,0 +1,9 @@
+        let clip = document.querySelector(".vid")
+  
+        clip.addEventListener("mouseover", function (e) {
+            clip.play();
+        })
+
+        clip.addEventListener("mouseout", function (e) {
+            clip.pause();
+        })
