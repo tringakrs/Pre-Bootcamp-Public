373937dffb94c11bc8eacb332c5451bca1428656
tringakrs <94629975+tringakrs@users.noreply.github.com>
2022-05-24 02:11:22 +0200

    Add files via upload

---
+++ Dojo/YellowBeltExam/Attempt 1/script.js
@@ -0,0 +1,15 @@
+function changeFirst(){
+    document.getElementById("plants").src = "assets/succulents-2.jpg";
+}
+
+function changeSecond(){
+    document.getElementById("plants").src = "assets/succulents-1.jpg";
+}
+
+function removeButton(){
+    document.getElementById("removedButton").remove()
+}
+
+function displayText(){
+    alert("You Cart is empty...")
+}
